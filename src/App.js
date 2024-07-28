f219ca3fc2710578585e17163d0cfb65596debe9
StefanPenkVivino <stefan.penk@vivino.com>
2024-07-28 12:56:55 +0200

    added home page, nav, help page, about page, faq page, contact page, added navlinks and routes

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import {
+  createBrowserRouter,
+  Routes,
+  Route,
+  Link,
+  NavLink,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
+import Faq from "./pages/help/Faq.jsx";
+import Contact from "./pages/help/Contact.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
